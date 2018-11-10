00cd367f39501da6e5573b9d746bffc6a46c7f9e
Piotr Gzubicki <piotr.gzubicki97@gmail.com>
2018-11-10 21:24:30 +0100

    Added models in MVC (Registrtation and Login)

---
+++ WeShare/WeShareClient2/Models/RegistrationModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WeShareClient2.Models
+{
+    public class RegistrationModel
+    {
+        public string CPR { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string ZipCode { get; set; }
+        public string City { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string FileName { get; set; }
+        public string Guid { get; set; }
+    }
+}
