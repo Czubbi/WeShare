69731bc904e8853c6784413723a1a6673be3f9f2
Piotr Gzubicki <piotr.gzubicki97@gmail.com>
2018-11-10 20:18:16 +0100

    Models in MVC

---
+++ WeShare/WeShareClient/Models/RegistrationModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WeShareClient.Models
+{
+    public class RegistrationModel
+    {
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public string FileName { get; set; }
+
+        public string Guid { get; set; }
+    }
+}
