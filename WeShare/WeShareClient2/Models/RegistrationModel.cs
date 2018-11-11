93ffe04fef126b78c64ff6da531a43fc77af639f
David Szoke <tropson90@gmail.com>
2018-11-11 08:55:35 +0100

    Google maps API done

--- WeShare/WeShareClient2/Models/RegistrationModel.cs
+++ WeShare/WeShareClient2/Models/RegistrationModel.cs
@@ -10,6 +10,8 @@ namespace WeShareClient2.Models
         public string CPR { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string AddressStreet { get; set; }
+        public string AddressNumber { get; set; }
         public string Address { get; set; }
         public string ZipCode { get; set; }
         public string City { get; set; }
